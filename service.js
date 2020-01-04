1e12c19c10de2916795846c1c52e019981ef0d60
Matt McElwee <mmcelwee@dmsi.com>
2020-01-04 10:43:52 -0600

    Modified service.db to open and close a connection on each query

--- service.js
+++ service.js
@@ -3,14 +3,22 @@ const mysql = require("promise-mysql");
 const { Storage } = require("@google-cloud/storage");
 
 module.exports = async () => {
-  const db = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    socketPath: process.env.SQL_INSTANCE_CONNECTION_NAME
-  });
+  const db = {
+    query: async (query) => {
+      const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        socketPath: process.env.SQL_INSTANCE_CONNECTION_NAME
+      });
+      
+      const data = await connection.query(query);
+      connection.end();
+      return data;
+    }
+  };
 
   const bucket = new Storage({
     projectId: "wwcd-2019"
